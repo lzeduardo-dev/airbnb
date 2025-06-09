e9575635756340165be08bdedc52e891dbbebec7
Luiz Eduardo <luizeduardomgm81@gmail.com>
2025-06-09 18:52:14 -0300

    feat(user): implementa rotas de  login

---
+++ back-end/domains/users/routes.js
@@ -0,0 +1,61 @@
+import { Router } from "express";
+import { connectDb } from "../../config/db.js";
+import User from "./model.js";
+import bcrypt from "bcryptjs";
+
+const router = Router();
+const bcryptSalt = bcrypt.genSaltSync();
+
+router.get("/", async (req, res) => {
+  connectDb();
+
+  try {
+    const userDoc = await User.find();
+
+    res.json(userDoc);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  connectDb();
+
+  const { name, email, password } = req.body;
+  const encryptedPassword = bcrypt.hashSync(password, bcryptSalt);
+
+  try {
+    const newUserDoc = await User.create({
+      name,
+      email,
+      password: encryptedPassword,
+    });
+
+    res.json(newUserDoc);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  connectDb();
+
+  const { email, password } = req.body;
+  
+  try {
+    const userDoc = await User.findOne({ email });
+    
+    if (userDoc) {
+      const passwordCorrect = bcrypt.compareSync(password, userDoc.password);
+      const  {name, _id} = userDoc;
+
+      passwordCorrect ? res.json({name,email,_id}) : res.json("Senha inválida!");
+    } else {
+      res.status(400).json("Usuário não encontrado");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+export default router;
