55b89a9540d0c2b3702e128a4d132fcf418efaa4
Luiz Eduardo <luizeduardomgm81@gmail.com>
2025-06-10 14:27:43 -0300

    feat(user): registra usuarios no banco de dados

---
+++ front-end/airbnb/src/Pages/Register.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Register = ({ setUser }) => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (email && password && name) {
+      try {
+        const { data: userDoc } = await axios.post("/users", {
+          name,
+          email,
+          password,
+        });
+        setUser(userDoc);
+        setRedirect(true);
+      } catch (error) {
+        alert(`Deu um erro ao cadastrar o usuário: ${JSON.stringify(error)}`);
+      }
+    } else {
+      alert("você precisa preencher o e-mail e a senha!");
+    }
+  };
+
+  if (redirect) return <Navigate to="/" />;
+
+  return (
+    <section className="flex items-center">
+      <div className="mx-auto max-w-96 w-full flex flex-col gap-4 p-8 items-center my-20">
+        <h1 className="font-bold text-3xl">Faça seu registro</h1>
+        <form
+          action=""
+          className="flex flex-col w-full gap-6"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            className="w-full rounded-full border border-gray-300 py-3 pr-4 pl-6 shadow-md"
+            placeholder="Digite seu nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            className="w-full rounded-full border border-gray-300 py-3 pr-4 pl-6 shadow-md"
+            placeholder="Digite seu email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            className="w-full rounded-full border border-gray-300 py-3 pr-4 pl-6 shadow-md"
+            placeholder="Digite sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="rounded-full w-full py-2 px-4 bg-rose-500 text-neutral-50 hover:cursor-pointer">
+            Resgistrar
+          </button>
+        </form>
+        <p className="">
+          Ja possui uma conta?
+          <Link to="/login">
+            <span className="underline">Logue aqui!</span>
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
