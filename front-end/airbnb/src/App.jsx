2ed053f73e8fdb0212243b6654a56a47d9568478
Luiz Eduardo <luizeduardomgm81@gmail.com>
2025-06-11 10:16:44 -0300

    feat(auth): adiciona contexto do usuário e integração com autenticação

--- front-end/airbnb/src/App.jsx
+++ front-end/airbnb/src/App.jsx
@@ -1,19 +1,41 @@
-import Header from "./Components/Header";
-import Item from "./Components/Item";
-import Home from "./Pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+import Header from "./Components/Header";
+import Home from "./Pages/Home";
 import Login from "./Pages/Login";
+import axios from "axios";
+import Register from "./Pages/Register";
+import Account from "./Pages/Account";
+import { UserContext } from "../Contexts/UserContext";
+
+axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.withCredentials = true;
+
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const axiosGet = async () => {
+      const { data } = await axios.get("/users/profile");
+      setUser(data);
+    };
+    axiosGet();
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path="/" element = {<Home/>}/>
-        <Route path=""/>
-        <Route path="/login" element ={<Login/>}/>
-      </Routes>
-    </BrowserRouter>
+    <UserContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <Header user={user} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login setUser={setUser} />} />
+          <Route path="/register" element={<Register setUser={setUser} />} />
+          <Route path="/account/:subpage?" element={<Account />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
-}
+} 
 
 export default App;
